f10ac85eca32f119fe0d750033d06e162a5f5054
smg02 <syedmdghouse02@gmail.com>
2024-05-16 11:40:19 +0530

    final

--- magic.js
+++ magic.js
@@ -16,34 +16,45 @@ let playRound = function (humanChoice, computerChoice) {
     let result;
     const container = document.querySelector(".container");
     const gameResult = document.createElement("div")
+    gameResult.classList="winner"
+    
+    let choicesShow = document.querySelector(".choices");
+    let resultShow = document.querySelector(".result");
+
+    let humanScoreDisplay = document.querySelector(".humanScoreDisplay");
+    let computerScoreDisplay = document.querySelector(".computerScoreDisplay");
 
     if (humanChoice === computerChoice) {
         result = "Draw!";
+        resultShow.setAttribute("style", "color:white");
     } else if (((humanChoice === "Rock") && (computerChoice === "Scissors")) ||
         ((humanChoice === "Paper") && (computerChoice === "Rock")) ||
         ((humanChoice === "Scissors") && (computerChoice === "Paper"))) {
         humanScore++;
         result = "You Win!";
+        resultShow.setAttribute("style", "color:#96c4b2");
     } else {
         computerScore++;
         result = "You Lose!";
+        resultShow.setAttribute("style", "color:#a9bdb9");
     }
-    let choicesShow = document.querySelector(".choices");
     choicesShow.textContent = `${humanChoice} vs ${computerChoice}`
 
-    let resultShow = document.querySelector(".result");
     resultShow.textContent = `${result}`
 
-    let scoreShow = document.querySelector(".score");
-    scoreShow.textContent = `Your Score - ${humanScore}, Computer Score - ${computerScore}`
+    humanScoreDisplay.textContent = `You - ${humanScore}`
+
+    computerScoreDisplay.textContent = `Opponent - ${computerScore}`
 
     if (humanScore >= 3 || computerScore >= 3) {
         if (computerScore == humanScore) {
             gameResult.textContent = "Tie"
         } else if (humanScore >= 3) {
             gameResult.textContent = "Victory!"
+            gameResult.setAttribute("style", "color:#6DC5D1");
         } else if (computerScore >= 3) {
             gameResult.textContent = "Defeat!"
+            gameResult.setAttribute("style", "color:#FF033E");
         }
         humanScore = 0;
         computerScore = 0;
